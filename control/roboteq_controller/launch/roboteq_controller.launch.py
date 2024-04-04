648206a3c0b003eca0a5d66aff1eb911e1014408
Yuki Akimoto <yuki_akimoto@jp.honda>
2024-04-04 23:12:06 +0900

    Add 'roboteq_controller' launch file

---
+++ control/roboteq_controller/launch/roboteq_controller.launch.py
@@ -0,0 +1,39 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    PACKAGE_NAME = "roboteq_controller"
+    launch_args = (
+        DeclareLaunchArgument(
+            "sub_speed_command",
+            default_value="/aiformula_control/game_pad/cmd_vel",
+            description="Topic name of speed command",
+        ),
+        DeclareLaunchArgument(
+            "pub_can",
+            default_value="/aiformula_control/roboteq_controller/reference_signal",
+            description="Topic name of sending can data",
+        ),
+    )
+
+    nodes = (
+        Node(
+            package=PACKAGE_NAME,
+            executable=PACKAGE_NAME,
+            name=PACKAGE_NAME,
+            namespace="/aiformula_control",
+            output="screen",
+            emulate_tty=True,
+            remappings=[
+                ("sub_speed_command", LaunchConfiguration("sub_speed_command")),
+                ("pub_can", LaunchConfiguration("pub_can")),
+            ],
+        ),
+    )
+    return LaunchDescription([
+        *launch_args,
+        *nodes,
+    ])
--- control/roboteq_controller/roboteq_controller/roboteq_controller.py
+++ control/roboteq_controller/roboteq_controller/roboteq_controller.py
@@ -8,25 +8,27 @@ from std_msgs.msg import String, Float64, UInt8, Int32
 from can_msgs.msg import Frame
 from decimal import Decimal, ROUND_HALF_DOWN
 
+
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.subscriber = self.create_subscription(Twist,'cmd_vel', self.callback, 10)
-        self.publisher_ = self.create_publisher(Frame, 'to_can_bus', 10)
+        self.subscriber = self.create_subscription(
+            Twist, 'sub_speed_command', self.callback, 10)
+        self.publisher_ = self.create_publisher(Frame, 'pub_can', 10)
         self.vel = Twist()
         self.cmd = Frame()
         self.diameter = 0.248
         self.tread = 0.6
-        self.gear_ratio=1
+        self.gear_ratio = 1
 
-    def callback(self,msg):
+    def callback(self, msg):
         self.vel = msg
         self.cmd_linear_x = self.vel.linear.x
         self.cmd_angular_z = self.vel.angular.z
 
         r_ref = (1 / (self.diameter / 2)) * self.cmd_linear_x + (
-         self.tread / self.diameter
+            self.tread / self.diameter
         ) * self.cmd_angular_z  # [rad/s]
         r_ref = r_ref * 30 / math.pi  # [rpm]  タイヤ1回転　30rpm
         r_ref = r_ref * self.gear_ratio  # [rpm]
@@ -37,23 +39,28 @@ class MinimalPublisher(Node):
         l_ref = l_ref * 30 / math.pi  # [rpm]
         l_ref = l_ref * self.gear_ratio  # [rpm]
         # convert cg command
-        r_ref = Decimal(str(r_ref)).quantize(Decimal("0"), rounding=ROUND_HALF_DOWN)
-        l_ref = Decimal(str(l_ref)).quantize(Decimal("0"), rounding=ROUND_HALF_DOWN)
-        r_byte = (int(r_ref)).to_bytes(4, "big", signed=True)  # high byteから順番に並んでいる
+        r_ref = Decimal(str(r_ref)).quantize(
+            Decimal("0"), rounding=ROUND_HALF_DOWN)
+        l_ref = Decimal(str(l_ref)).quantize(
+            Decimal("0"), rounding=ROUND_HALF_DOWN)
+        r_byte = (int(r_ref)).to_bytes(
+            4, "big", signed=True)  # high byteから順番に並んでいる
         l_byte = (int(l_ref)).to_bytes(4, "big", signed=True)
-        r_cmd = [int(r_byte[0]), int(r_byte[1]), int(r_byte[2]), int(r_byte[3])]
-        l_cmd = [int(l_byte[0]), int(l_byte[1]), int(l_byte[2]), int(l_byte[3])] 
+        r_cmd = [int(r_byte[0]), int(r_byte[1]),
+                 int(r_byte[2]), int(r_byte[3])]
+        l_cmd = [int(l_byte[0]), int(l_byte[1]),
+                 int(l_byte[2]), int(l_byte[3])]
 #        self.cmd.header(
 #            header=0x210, can_data=[l_cmd[3], l_cmd[2], l_cmd[1], l_cmd[0], r_cmd[3], r_cmd[2], r_cmd[1], r_cmd[0]]
 #        )
-        self.cmd.header.frame_id="can0"
-        self.cmd.id=0x210
-        self.cmd.dlc= 8
-        self.cmd.data=[l_cmd[3], l_cmd[2], l_cmd[1], l_cmd[0], r_cmd[3], r_cmd[2], r_cmd[1], r_cmd[0]]
+        self.cmd.header.frame_id = "can0"
+        self.cmd.id = 0x210
+        self.cmd.dlc = 8
+        self.cmd.data = [l_cmd[3], l_cmd[2], l_cmd[1],
+                         l_cmd[0], r_cmd[3], r_cmd[2], r_cmd[1], r_cmd[0]]
         self.publisher_.publish(self.cmd)
- 
 
- 
+
 def main(args=None):
     rclpy.init(args=args)
 
@@ -66,6 +73,7 @@ def main(args=None):
     # when the garbage collector destroys the node object)
     minimal_publisher.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
