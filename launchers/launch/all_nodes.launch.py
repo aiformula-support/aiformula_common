d5c92bebae6fedc20e7deb0a52c207d2f69f127c
Yuki Akimoto <yuki_akimoto@jp.honda>
2024-04-08 07:22:46 +0900

    Remove unused import in all_nodes.launch.py

--- launchers/launch/all_nodes.launch.py
+++ launchers/launch/all_nodes.launch.py
@@ -3,7 +3,6 @@ import yaml
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 
