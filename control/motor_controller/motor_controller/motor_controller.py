44b5813cdf3e4e640fa1c5c0f3951966b49d4c3c
masayaokada08 <masayaokada@jp.honda>
2024-04-09 20:07:58 +0900

    modify msg name

--- control/motor_controller/motor_controller/motor_controller.py
+++ control/motor_controller/motor_controller/motor_controller.py
@@ -12,34 +12,34 @@ class MotorController(Node):
 
     def __init__(self):
         super().__init__('motor_controller')
+        self.declare_parameter('wheel.tread')
+        self.declare_parameter('wheel.diameter')
+        self.declare_parameter('wheel.gear_ratio')
+        self.declare_parameter('publish_timer_loop_duration')
+        self.tread = self.get_parameter('wheel.tread').get_parameter_value().double_value
+        self.diameter = self.get_parameter('wheel.diameter').get_parameter_value().double_value
+        self.gear_ratio = self.get_parameter('wheel.gear_ratio').get_parameter_value().double_value
+        self.publish_timer_loop_duration = self.get_parameter(
+            'publish_timer_loop_duration').get_parameter_value().double_value
+
         buffer_size = 10
         self.twist_sub = self.create_subscription(Twist, 'sub_speed_command', self.twist_callback, buffer_size)
         self.can_pub = self.create_publisher(Frame, 'pub_can', buffer_size)
-        self.cmd = Frame()
-        self.declare_parameter('motor.tread')
-        self.declare_parameter('motor.diameter')
-        self.declare_parameter('motor.gear_ratio')
-        self.declare_parameter('motor.publish_timer_loop_duration')
-        self.tread = self.get_parameter('motor.tread').get_parameter_value().double_value
-        self.diameter = self.get_parameter('motor.diameter').get_parameter_value().double_value
-        self.gear_ratio = self.get_parameter('motor.gear_ratio').get_parameter_value().double_value
-        self.publish_timer_loop_duration = self.get_parameter(
-            'motor.publish_timer_loop_duration').get_parameter_value().double_value
-        self.publish_timer = self.create_timer(self.publish_timer_loop_duration, self.canframe_callback)
+        self.publish_timer = self.create_timer(self.publish_timer_loop_duration, self.publish_canframe_callback)
+        self.frame_msg = Frame()
 
     def twist_callback(self, msg):
-        self.vel = msg
-        ref = self.toRefRPM(self.vel.linear.x, self.vel.angular.z)
-        l_cmd = self.toCanCmd(ref[0])
-        r_cmd = self.toCanCmd(ref[1])
-        can_data = l_cmd + r_cmd
-        self.cmd.header.frame_id = "can0"        # Default can0
-        self.cmd.id = 0x210                      # MotorControler CAN ID : 0x210
-        self.cmd.dlc = 8                         # Data length
-        self.cmd.data = can_data
+        rpm = self.toRefRPM(msg.linear.x, msg.angular.z)
+        cmd_right = self.toCanCmd(rpm[0])
+        cmd_left = self.toCanCmd(rpm[1])
+        can_data = cmd_right + cmd_left
+        self.frame_msg.header.frame_id = "can0"        # Default can0
+        self.frame_msg.id = 0x210                      # MotorControler CAN ID : 0x210
+        self.frame_msg.dlc = 8                         # Data length
+        self.frame_msg.data = can_data
 
-    def canframe_callback(self):
-        self.can_pub.publish(self.cmd)
+    def publish_canframe_callback(self):
+        self.can_pub.publish(self.frame_msg)
 
 #  Velocity -> RPM Calc
 #  V_right = (V + tread/2 * w), V_left = (V - tread / 2 * w ) [m/s]
@@ -51,9 +51,8 @@ class MotorController(Node):
     def toRefRPM(self, linear_velocity, angular_velocity):  # Calc Motor ref rad/s
         wheel_angular_velocities = np.array([(linear_velocity / (self.diameter * 0.5)) + (self.tread / self.diameter) * angular_velocity,  # right[rad/s]
                                              (linear_velocity / (self.diameter * 0.5)) - (self.tread / self.diameter) * angular_velocity])  # left[rad/s]
-        min = 60
-        rpm = wheel_angular_velocities * (min / (2 * math.pi))
-        print(rpm)
+        minute_to_second = 60
+        rpm = wheel_angular_velocities * (minute_to_second / (2 * math.pi))
         return (rpm * self.gear_ratio).tolist()
 
     @staticmethod
