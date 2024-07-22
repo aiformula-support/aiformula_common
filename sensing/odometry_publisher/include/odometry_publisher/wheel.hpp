6d9262f704e2b38778b32893656b0bb8015827a2
Yuki Akimoto <yuki_akimoto@jp.honda>
2024-07-22 18:42:29 +0900

    Refactor to improve readability in wheel.hpp

--- sensing/odometry_publisher/include/odometry_publisher/wheel.hpp
+++ sensing/odometry_publisher/include/odometry_publisher/wheel.hpp
@@ -13,15 +13,16 @@ namespace odometry_publisher {
 union Uint8ArrayToInt {
     uint8_t arr[4];
     int val;
-    Uint8ArrayToInt(uint8_t input_arr[4]) { std::memcpy(arr, input_arr, sizeof(arr)); }
+    Uint8ArrayToInt(uint8_t arr_in[4]) { std::memcpy(arr, arr_in, sizeof(arr)); }
 };
 
 template <typename T>
 struct DriveWheel {
-    explicit DriveWheel(const T& left, const T& right) : left(left), right(right) {}
+    explicit DriveWheel(const T& left_in, const T& right_in) : left(left_in), right(right_in) {}
     DriveWheel<double> operator*(const double& rhs) const { return DriveWheel<double>(left * rhs, right * rhs); }
     T left, right;
 };
+
 const double SECOND_TO_MINUTE = 0.016667;  // = 1/60
 const double DEGREE_TO_RADIAN = M_PI / 180.0;
 const double RADIAN_TO_DEGREE = 180.0 / M_PI;
@@ -30,8 +31,9 @@ const int RPM_ID = 1809;
 
 inline DriveWheel<double> calcEachWheelVelocity(const can_msgs::msg::Frame::SharedPtr& can_msg,
                                                 const double& wheel_diameter) {
-    const DriveWheel<Uint8ArrayToInt> rpm_union(&can_msg->data[4], &can_msg->data[0]);
-    const DriveWheel<int> rpm(static_cast<int>(rpm_union.left.val), static_cast<int>(rpm_union.right.val));
+    // Right: can_msg->data[0]-[3],  Left: can_msg->data[4]-[7]
+    const DriveWheel<Uint8ArrayToInt> rpm_union(&(can_msg->data[4]), &(can_msg->data[0]));
+    const DriveWheel<int> rpm(rpm_union.left.val, rpm_union.right.val);
     const auto wheel_circumference = wheel_diameter * M_PI;
     return rpm * SECOND_TO_MINUTE * wheel_circumference;
 }
