1087b18a03ea7ec66076b3e3e3e8890b704cc149
Yuki Akimoto <yuki_akimoto@jp.honda>
2024-09-19 14:27:28 +0900

    Refactor custom exception in `compressed_image_viewer`.

--- remote_control/compressed_image_viewer/include/compressed_image_viewer/exception.hpp
+++ remote_control/compressed_image_viewer/include/compressed_image_viewer/exception.hpp
@@ -6,21 +6,21 @@
 
 namespace aiformula {
 
-class ScreenIndexException : public BasicException {
+class ScreenIndexException : public std::runtime_error {
 public:
     ScreenIndexException(const int& target_screen_idx)
-        : BasicException("'target_screen_idx_'(=" + std::to_string(target_screen_idx) +
-                         ") must be greater than or equal to 0.") {}
+        : std::runtime_error("'target_screen_idx_'(=" + std::to_string(target_screen_idx) +
+                             ") must be greater than or equal to 0.") {}
     ScreenIndexException(const int& target_screen_idx, const int& num_monitors)
-        : BasicException("'target_screen_idx_'(=" + std::to_string(target_screen_idx) +
-                         ") must be an integer between 0 and " + std::to_string(num_monitors - 1) + ".") {}
+        : std::runtime_error("'target_screen_idx_'(=" + std::to_string(target_screen_idx) +
+                             ") must be an integer between 0 and " + std::to_string(num_monitors - 1) + ".") {}
 };
 
-class WindowWidthRatioException : public BasicException {
+class WindowWidthRatioException : public std::runtime_error {
 public:
     WindowWidthRatioException(const double& window_width_ratio)
-        : BasicException("'window_width_ratio_'(=" + std::to_string(window_width_ratio) +
-                         ") must be greater than 0.0.") {}
+        : std::runtime_error("'window_width_ratio_'(=" + std::to_string(window_width_ratio) +
+                             ") must be greater than 0.0.") {}
 };
 
 }  // namespace aiformula
